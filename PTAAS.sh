b4600677217c0a2c32d09e5ce4a7f1d1af042f10
kishangupta94 <kishangupta9494@gmail.com>
2022-09-26 14:51:40 +0530

    PTAAS PROJECT

--- PTAAS.sh
+++ PTAAS.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-bash launch_burp.sh http testphp.vulweb.com 80 / kishangupta9494@gmail.com --user-config-file=user.json
+bash launch_burp.sh http assets.winni.in 80 / kishangupta9494@gmail.com --user-config-file=user.json
